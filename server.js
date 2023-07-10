99a3a8fdff0e6e22585ef3e98cc4b926a24011dd
Joseph Peter <youfeat2000@gmail.com>
2023-07-10 15:49:12 +0100

    worked on the frontend

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = 3500;
 const app = express();
 require("dotenv").config();
 //const db = "mongodb://127.0.0.1:27017/youfeat";
+uri = "https://youfeat-client.onrender.com";
+//uri = "http://localhost:3000";
 const db = process.env.DB_URL;
 mongoose
   .connect(db, {
@@ -25,7 +27,7 @@ app.use(cookieparser());
 app.use(
   cors({
     credentials: true,
-    origin: "https://youfeat-client.onrender.com",
+    origin: uri,
   })
 );
 
