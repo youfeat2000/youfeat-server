3d5592aafc1cf5449ac889e7a42b7f1358c9f06c
Joseph Peter <youfeat2000@gmail.com>
2023-08-12 16:06:03 +0100

    worked on uploads

--- controler/handleProfileImage.js
+++ controler/handleProfileImage.js
@@ -3,6 +3,15 @@ const User = require("../schema/userSchema");
 const handleProfileImage = (req, res) => {
   const { filename } = req.file;
   const { userid } = req.params;
+
+  User.findById(userid)
+    .then((data) => {
+      console.log(data);
+      if (data.profileImage) {
+        fs.unlinkSync(`Images/${data.profileImage}`);
+      }
+    })
+    .catch((err) => console.log(err));
   User.findByIdAndUpdate(userid, { profileImage: filename })
     .then((data) => res.send(data))
     .catch((err) => res.sendStatus(400));
--- controler/handleVideoUploads.js
+++ controler/handleVideoUploads.js
@@ -1,7 +1,16 @@
+const fs = require("fs");
 const User = require("../schema/userSchema");
 
 // Function to upload video using multer and mongoose
 const uploadVideo = (req, res) => {
+  User.findById(req.body.userId)
+    .then((data) => {
+      console.log(data);
+      if (data.video) {
+        fs.unlinkSync(`videos/${data.video.filename}`);
+      }
+    })
+    .catch((err) => console.log(err));
   User.findByIdAndUpdate(req.body.userId, {
     video: {
       filename: req.file.filename,
@@ -10,7 +19,7 @@ const uploadVideo = (req, res) => {
       catigory: req.body.catigory,
       description: req.body.description,
     },
-  }).then(() => res.sendStatus(200));
+  }).then((data) => res.status(200).send(data));
 };
 
 module.exports = uploadVideo;
